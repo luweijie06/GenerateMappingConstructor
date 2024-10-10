1852e16c0b922ce50ffc81589394ba50313e2154
luwejie06 <1397100450@qq.com>
2024-10-10 23:33:20 +0800

    feat: 修改初始化加载

--- src/main/java/com/dev/gear/PluginInitializer.java
+++ src/main/java/com/dev/gear/PluginInitializer.java
@@ -1,20 +1,12 @@
-            
 package com.dev.gear;
 
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.startup.ProjectActivity;
-import groovyjarjarantlr4.v4.runtime.misc.NotNull;
-import groovyjarjarantlr4.v4.runtime.misc.Nullable;
-import kotlin.Unit;
-import kotlin.coroutines.Continuation;
+import com.intellij.openapi.startup.StartupActivity;
+import org.jetbrains.annotations.NotNull;
 
-public class PluginInitializer implements ProjectActivity {
-    @Nullable
+public class PluginInitializer implements StartupActivity {
     @Override
-    public Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
+    public void runActivity(@NotNull Project project) {
         ClassChooserUtil.initialize(project);
-        return Unit.INSTANCE;
     }
 }
-
-        
